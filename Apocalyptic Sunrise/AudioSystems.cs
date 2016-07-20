25aac977e01904f0082ff3334c4174a27c096ad6
ILM126 <ILM126@hotmail.com.au>
2016-07-20 15:51:56 +0930

    Added: "DevLogging" and "AudioSystems" into this branch

---
+++ Apocalyptic Sunrise/AudioSystems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Apocalyptic_Sunrise
+{
+    class AudioSystems
+    {
+
+
+        public void Update()
+        {
+
+        }
+    }
+
+    class LoadMusic
+    {
+        
+    }
+}
