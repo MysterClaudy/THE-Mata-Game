98561ba846968b64b4773b6c75d65c721e473fb7
ILM126 <ILM126@hotmail.com.au>
2016-07-20 15:54:02 +0930

    Worked on implmenting Debug systems and organised code, slightly. But still works the same

--- Apocalyptic Sunrise/Game1.cs
+++ Apocalyptic Sunrise/Game1.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.IO;
-
+using TrebleGameUtils;
 
 namespace Apocalyptic_Sunrise
 {
@@ -13,31 +14,43 @@ namespace Apocalyptic_Sunrise
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        public static Game1 theGame;
+        public DevLogging Debug;
         public Player player;
         public Camera camera;
         GameStates gameStates;
         Level level;
         HealthBar healthBar;
-        public static Game1 theGame;
+
+        string GameVersionBuild;
         private int levelIndex = -1;
         private const int numberOfLevels = 2;
         Vector2 spawningPosition;
-
             
         public Game1()
         {
+            Debug = new DevLogging();
+            File.Delete(Debug.GetCurrentDirectory());
+            Debug.WriteToFile("This game proudly uses the TrebleSketch Utilities Debugger v6.2", true, false);
+            GameVersionBuild = "v0.3.3.81 ";
+            DateTime thisDay = DateTime.Now;
+            Debug.WriteToFile("Starting Apocalyptic Sunrise " + GameVersionBuild + thisDay.ToString("dd-MM-yyyy HH:mm:ss zzz"), true, false);
+
             graphics = new GraphicsDeviceManager(this);
-            
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
+
             theGame = this;
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            IsMouseVisible = true;
-            graphics.PreferredBackBufferHeight = 720;
-            graphics.PreferredBackBufferWidth = 1280;
-            graphics.ApplyChanges();
+            Debug.WriteToFile("Started Initializing Game", true, false);
+
             gameStates = new GameStates();
             level = new Level();
             player = new Player(new Vector2(100,100));
@@ -45,17 +58,20 @@ namespace Apocalyptic_Sunrise
             healthBar = new HealthBar(Content);
             //map = new TiledMap(GraphicsDevice, 110, 110, 32, 32, TiledMapOrientation.Orthogonal);
             base.Initialize();
-        }
 
+            Debug.WriteToFile("Finished Initializing Game", true, false);
+        }
        
         protected override void LoadContent()
         {
+            Debug.WriteToFile("Started Loading Game Textures", true, false);
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
             gameStates.LoadContent(Content);
             player.LoadContent(Content);
             level.LoadContent(Content);
-            
+
+            Debug.WriteToFile("Finished Loading Game Textures", true, false);
         }
 
         protected override void UnloadContent()
@@ -64,6 +80,12 @@ namespace Apocalyptic_Sunrise
 
         protected override void Update(GameTime gameTime)
         {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Debug.WriteToFile("Ending Game...", true, false);
+                Exit();
+            }
+
             gameStates.player = player;
             player.level = level;
             gameStates.level = level;
@@ -72,8 +94,6 @@ namespace Apocalyptic_Sunrise
             camera.Update(player.sPosition);
             gameStates.healthBar = healthBar;   
             gameStates.Update(gameTime);
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
 
             base.Update(gameTime);
         }
